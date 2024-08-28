c31514d41e8504f379e3cb73054b4b5718deb3df
hattrick <dasha.khitrik@gmail.com>
2024-08-28 16:55:45 +0300

    feat: add header

---
+++ src/ui/themeSwitcher/index.tsx
@@ -0,0 +1,33 @@
+import { bindActionCreators } from '@reduxjs/toolkit'
+import { useCallback, useContext } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { Input, Label, SwitcherBody, SwitcherButton } from './styled'
+import { ThemeMode } from '@/constants/theme'
+import { selectTheme } from '@/store/selectors'
+import { setDarkTheme, setLightTheme } from '@/store/slices/themeSlice'
+
+export const ThemeSwitcher = () => {
+    const dispatch = useDispatch()
+    const theme = useSelector(selectTheme)
+    const actions = bindActionCreators({ setDarkTheme, setLightTheme }, dispatch)
+
+    const switcherClickHandler = useCallback(() => {
+        theme === ThemeMode.dark ? actions.setLightTheme() : actions.setDarkTheme()
+    }, [theme])
+
+    return (
+        <>
+            <Label htmlFor='themeSwitcher' id='themeLabel'>
+                <Input
+                    onClick={switcherClickHandler}
+                    type='checkbox'
+                    id='themeSwitcher'
+                    defaultChecked={theme === ThemeMode.dark}
+                />
+                <SwitcherBody />
+                <SwitcherButton />
+            </Label>
+        </>
+    )
+}
